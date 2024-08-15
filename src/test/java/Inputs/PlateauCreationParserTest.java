456edfbf04390d3c4155289df49af0909970056b
Liam O'Hara <liam.ohara@gmail.com>
2024-08-15 12:13:29 +0100

    Test: implement further test condition for PlateauCreation parse with valid input.

--- src/test/java/Inputs/PlateauCreationParserTest.java
+++ src/test/java/Inputs/PlateauCreationParserTest.java
@@ -12,15 +12,20 @@ class PlateauCreationParserTest {
     void testPlateauCreationParserParseInput_WithValidInput() {
         // Arrange
         String plateauInput = "5 5";
+        String plateauInput2 = "22";
         PlateauSize plateauSize = new PlateauSize(5, 5);
+        PlateauSize plateauSize2 = new PlateauSize(2, 2);
 
         // Act
         PlateauSize result = PlateauCreationParser.parseInput(plateauInput);
+        PlateauSize result2 = PlateauCreationParser.parseInput(plateauInput2);
 
         // Assert
-        assertEquals(plateauSize, result);
-
-
+        assertAll(
+                () -> assertEquals(plateauSize, result),
+                () -> assertEquals(plateauSize2, result2));
 
     }
+
+
 }
