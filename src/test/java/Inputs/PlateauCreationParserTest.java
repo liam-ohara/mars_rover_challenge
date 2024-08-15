c80083cb043c18e1d0854f54ec70f6034ad5d0ef
Liam O'Hara <liam.ohara@gmail.com>
2024-08-15 14:51:24 +0100

    Test: implement test for PlateauCreationParser when provided with an input for large plateau.

--- src/test/java/Inputs/PlateauCreationParserTest.java
+++ src/test/java/Inputs/PlateauCreationParserTest.java
@@ -67,6 +67,27 @@ class PlateauCreationParserTest {
 
     }
 
+    @Test
+    @DisplayName("Returns PlateauSize instance with same attributes when given input string for large plateau")
+    void testPlateauCreationParserParseInput_ForLargePlateau() {
+
+        // Arrange
+        String plateauInput = "22 22";
+        String plateauInput2 = "1000 1000";
+        PlateauSize plateauSize = new PlateauSize(22, 22);
+        PlateauSize plateauSize2 = new PlateauSize(1000, 1000);
+
+        // Act
+        PlateauSize result = PlateauCreationParser.parseInput(plateauInput);
+        PlateauSize result2 = PlateauCreationParser.parseInput(plateauInput2);
+
+        // Assert
+        assertAll(
+                () -> assertEquals(plateauSize, result),
+                () -> assertEquals(plateauSize2, result2));
+
+    }
+
 
 
 }
