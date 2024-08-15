4257a010715edbc2d26d1721c624a3341abb02d8
Liam O'Hara <liam.ohara@gmail.com>
2024-08-15 10:48:56 +0100

    Feat: implement constructor and getter method for PlateauSize.

--- src/main/java/Inputs/PlateauSize.java
+++ src/main/java/Inputs/PlateauSize.java
@@ -3,4 +3,17 @@ package Inputs;
 public class PlateauSize {
     private int xSize;
     private int ySize;
+
+    public PlateauSize(int xSize, int ySize) {
+        this.xSize = xSize;
+        this.ySize = ySize;
+    }
+
+    public int getXSize() {
+        return xSize;
+    }
+
+    public int getYSize() {
+        return ySize;
+    }
 }
