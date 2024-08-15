d8b85545aa71904ba9d44c3c8813bbcd3e453f14
Liam O'Hara <liam.ohara@gmail.com>
2024-08-15 10:43:17 +0100

    Feat: implement constructor and getter method for PlateauCreationParser

--- src/main/java/Inputs/PlateauCreationParser.java
+++ src/main/java/Inputs/PlateauCreationParser.java
@@ -1,6 +1,17 @@
 package Inputs;
 
 public class PlateauCreationParser implements InputParser {
+
+    private PlateauSize plateauSize;
+
+    public PlateauCreationParser(PlateauSize plateauSize) {
+        this.plateauSize = plateauSize;
+    }
+
+    public PlateauSize getPlateauSize() {
+        return plateauSize;
+    }
+
     @Override
     public void parseInput(String input) {
 
