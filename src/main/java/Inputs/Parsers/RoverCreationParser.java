1166673945d2d0bb3480597ee2563b567d0e3d69
Liam O'Hara <liam.ohara@gmail.com>
2024-08-15 17:11:14 +0100

    Feat: implement logic in RoverCreationParser to return Position object when passed valid inputs.

--- src/main/java/Inputs/Parsers/RoverCreationParser.java
+++ src/main/java/Inputs/Parsers/RoverCreationParser.java
@@ -1,12 +1,40 @@
 package Inputs.Parsers;
 
+import Inputs.CompassDirection;
 import Inputs.Position;
 
-public class RoverCreationParser  {
+public class RoverCreationParser {
 
     public static Position parseInput(String input) {
+        int x = 0;
+        int y = 0;
+        CompassDirection facing = null;
 
-        return null;
+
+        if (input.isBlank()) {
+            return null;
+        }
+        String[] inputArray = input.split("\\s");
+        x = Integer.parseInt(inputArray[0]);
+        y = Integer.parseInt(inputArray[1]);
+        switch (inputArray[2]) {
+            case "N":
+                facing = CompassDirection.N;
+                break;
+            case "E":
+                facing = CompassDirection.E;
+                break;
+            case "S":
+                facing = CompassDirection.S;
+                break;
+            case "W":
+                facing = CompassDirection.W;
+        }
+
+        Position roverPosition = new Position(x, y, facing);
+
+        return roverPosition;
+
 
     }
 }
