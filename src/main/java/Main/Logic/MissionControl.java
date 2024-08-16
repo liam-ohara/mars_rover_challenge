13ee03b06356d79f6958c99acb5f3c5764ac75ef
Liam O'Hara <liam.ohara@gmail.com>
2024-08-16 09:27:48 +0100

    Feat: implement MissionControl class.

---
+++ src/main/java/Main/Logic/MissionControl.java
@@ -0,0 +1,12 @@
+package Main.Logic;
+
+import Main.Position;
+
+public class MissionControl {
+
+    public static boolean isPositionEmpty (Position position) {
+
+        return false;
+    }
+
+}
