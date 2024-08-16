b9e0e0cd58e360163926adbc6900b43d4f6891cc
Liam O'Hara <liam.ohara@gmail.com>
2024-08-16 11:27:06 +0100

    Test: implement test for addRover when provided with single rover.

--- src/test/java/Main/Logic/PlateauTest.java
+++ src/test/java/Main/Logic/PlateauTest.java
@@ -1,17 +1,21 @@
 package Main.Logic;
 
+import Main.PlateauSize;
+import Main.Position;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static Main.CompassDirection.N;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlateauTest {
 
     @Test
     @DisplayName("Returns null when there are no rovers added to listOfRovers in Plateau class.")
-    void testGetListOfRovers () {
+    void testGetListOfRovers_WithNoRovers () {
 
         //Act
         List<Rover> result = Plateau.getListOfRovers();
@@ -22,6 +26,22 @@ class PlateauTest {
 
 
     @Test
-    void addRover() {
+    @DisplayName("Returns list containing single rover when passed a single rover.")
+    void testAddRover_WithSingleRover() {
+        //Arrange
+        PlateauSize plateauSize = new PlateauSize(10, 10);
+        Plateau plateau = new Plateau("Icaria Planum", plateauSize);
+        Position roverLandingSite = new Position(0, 0, N);
+        Rover rover = new Rover("Pathfinder", roverLandingSite);
+        List<Rover> listOfRovers = new ArrayList<>();
+        listOfRovers.add(rover);
+
+        //Act
+        plateau.addRover(rover);
+        List<Rover> result = Plateau.getListOfRovers();
+
+        //Assert
+        assertEquals(listOfRovers, result);
+
     }
 }
