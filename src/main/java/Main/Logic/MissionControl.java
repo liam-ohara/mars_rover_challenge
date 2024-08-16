2d08bb02e9e1338f357d50771a38410f412ef709
Liam O'Hara <liam.ohara@gmail.com>
2024-08-16 14:22:46 +0100

    Feat: implement logical in isPositionEmpty when coordinates passed in a plateau with rovers are empty.

--- src/main/java/Main/Logic/MissionControl.java
+++ src/main/java/Main/Logic/MissionControl.java
@@ -1,13 +1,46 @@
 package Main.Logic;
 
+import Main.Position;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class MissionControl {
 
-    public static boolean isPositionEmpty (Plateau plateauName, int x, int y) {
+    public static boolean isPositionEmpty(Plateau plateauName, int x, int y) {
 
         if (plateauName.getListOfRovers() == null) {
             return true;
+        } else {
+            Map<Integer, Integer> occupiedCoordinates = new HashMap<>();
+            List<String> roverNames = new ArrayList<>();
+            List<Rover> listofRovers = new ArrayList<>();
+            List<Position> occupiedPositions = new ArrayList<>();
+            listofRovers = plateauName.getListOfRovers();
+
+            for (Rover i : listofRovers) {
+                roverNames.add(i.getRoverName());
+            }
+
+            for (Rover j : listofRovers) {
+                occupiedPositions.add(j.getRoverPosition());
+            }
+
+            for (Position k : occupiedPositions) {
+                occupiedCoordinates.put(k.x(), k.y());
+            }
+
+            if (!occupiedCoordinates.containsKey(x)) {
+                return true;
+            } else {
+                if (occupiedCoordinates.get(x) != y) {
+                    return true;
+                }
+            }
         }
         return false;
     }
-
 }
