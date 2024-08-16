700abf20a223b766eb9064274c40740cfdc1fc4a
Liam O'Hara <liam.ohara@gmail.com>
2024-08-16 11:32:54 +0100

    Test: implement test for addRover when provided with multiple rovers.

--- src/test/java/Main/Logic/PlateauTest.java
+++ src/test/java/Main/Logic/PlateauTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import java.util.ArrayList;
 import java.util.List;
 
-import static Main.CompassDirection.N;
+import static Main.CompassDirection.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlateauTest {
@@ -38,6 +38,34 @@ class PlateauTest {
 
         //Act
         plateau.addRover(rover);
+        List<Rover> result = Plateau.getListOfRovers();
+
+        //Assert
+        assertEquals(listOfRovers, result);
+
+    }
+
+    @Test
+    @DisplayName("Returns list containing multiple rovers when passed multiple rovers.")
+    void testAddRover_WithMultipleRovers() {
+        //Arrange
+        PlateauSize plateauSize = new PlateauSize(10, 10);
+        Plateau plateau = new Plateau("Icaria Planum", plateauSize);
+        Position roverLandingSite = new Position(0, 0, N);
+        Position roverLandingSite2 = new Position(5, 5, S);
+        Position roverLandingSite3 = new Position(10, 10, E);
+        Rover rover = new Rover("Pathfinder", roverLandingSite);
+        Rover rover2 = new Rover("Opportunity", roverLandingSite);
+        Rover rover3 = new Rover("Spirit", roverLandingSite);
+        List<Rover> listOfRovers = new ArrayList<>();
+        listOfRovers.add(rover);
+        listOfRovers.add(rover2);
+        listOfRovers.add(rover3);
+
+        //Act;
+        plateau.addRover(rover);
+        plateau.addRover(rover2);
+        plateau.addRover(rover3);
         List<Rover> result = Plateau.getListOfRovers();
 
         //Assert
